462597b6fad305f56ed5e1330b1bceaa83cecbf0
Aditya Gaur <gauradi355@gmail.com>
2024-10-09 22:00:27 +0530

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-$(document).ready(function () {
-    $("a").on("click", function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
+// $(document).ready(function () {
+//     $("a").on("click", function (event) {
+//       if (this.hash !== "") {
+//         event.preventDefault();
   
-        var hash = this.hash;
-        $("html, body").animate(
-          {
-            scrollTop: $(hash).offset().top,
-          },
-          800,
-          function () {
-            window.location.hash = hash;
-          }
-        );
-      }
+//         var hash = this.hash;
+//         $("html, body").animate(
+//           {
+//             scrollTop: $(hash).offset().top,
+//           },
+//           800,
+//           function () {
+//             window.location.hash = hash;
+//           }
+//         );
+//       }
+//     });
+//   });
+  
+//   $(".menu-items a").click(function () {
+//     $("#checkbox").prop("checked", false);
+//   });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let links = document.querySelectorAll("a");
+    links.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+
+                let hash = this.hash;
+                let target = document.querySelector(hash);
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: "smooth"
+                });
+                window.location.hash = hash;
+            }
+        });
     });
-  });
-  
-  $(".menu-items a").click(function () {
-    $("#checkbox").prop("checked", false);
-  });
+
+    let menuItems = document.querySelectorAll(".menu-items a");
+    menuItems.forEach(function (item) {
+        item.addEventListener("click", function () {
+            document.getElementById("checkbox").checked = false;
+        });
+    });
+});
